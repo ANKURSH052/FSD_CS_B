33217bae2ccb6363abe0ffa47c5e0d4ae75c14c8
ankursh052 <ankursh052@gmail.com>
2024-12-17 21:14:13 +0530

    restaurantapp

---
+++ RestaurantApp/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>
+        <center>Design and Developed by Ankur Sharma</center>
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
